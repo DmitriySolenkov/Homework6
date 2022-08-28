812b94f47a107129c6a2d2e49cafc9557e996721
DmitriySolenkov <soledimk@gmail.com>
2022-08-28 12:54:32 +0300

    Ex43Format

--- Ex43/Program.cs
+++ Ex43/Program.cs
@@ -1,26 +1,26 @@
 ﻿
 Console.Write("Введите k1: ");
-double k1= Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b1: ");
-double b1= Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите k2: ");
-double k2= Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b2: ");
-double b2= Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 
-if (k1==k2 && b1==b2)
+if (k1 == k2 && b1 == b2)
 {
-Console.WriteLine("Прямые совпадают");
+    Console.WriteLine("Прямые совпадают");
 }
 
-if (k1==k2 && b1!=b2)
+if (k1 == k2 && b1 != b2)
 {
-Console.WriteLine("Прямые параллельны");
+    Console.WriteLine("Прямые параллельны");
 }
 
-if (k1!=k2)
+if (k1 != k2)
 {
-    double resX= (b2-b1)/(k1-k2); 
-    double resY= k1*resX+b1;
+    double resX = (b2 - b1) / (k1 - k2);
+    double resY = k1 * resX + b1;
     Console.WriteLine($"Точка пересечения прямых: x={resX}, y={resY}");
 }
